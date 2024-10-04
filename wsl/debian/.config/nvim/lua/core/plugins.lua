ee753e6680d1cc36a3960a21256e06d95f8cacf7
data-miner00 <data-miner00@users.noreply.github.com>
2024-10-04 19:59:11 +0800

    feat(nvim): mini.ai plugin

--- wsl/debian/.config/nvim/lua/core/plugins.lua
+++ wsl/debian/.config/nvim/lua/core/plugins.lua
@@ -54,6 +54,8 @@ return require('packer').startup(function(use)
     'nvimdev/dashboard-nvim',
     requires = { 'nvim-tree/nvim-web-devicons' }
   }
+  use 'echasnovski/mini.ai'
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if packer_bootstrap then
