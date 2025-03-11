aef86966d1b3173f9e3adc80ada197889ad8b932
Shaun Chong <data-miner00@users.noreply.github.com>
2025-03-11 13:19:55 +0800

    feat(pwsh): Add name parameter for env script

--- .config/pwsh/modules/Get-EnvironmentVariables.psm1
+++ .config/pwsh/modules/Get-EnvironmentVariables.psm1
@@ -3,8 +3,24 @@
     Retrieves all environment variables on the computer.
 .Description
     Gets all environment variables set on the computer sorted by name in ascending order.
+.Parameter Name
+    The name of the environment variable to retrieve.
+.Example
+    Get-EnvironmentVariables
+.Example
+    Get-EnvironmentVariables -Name "Path"
 #>
 function Get-EnvironmentVariables {
+    Param (
+        [Parameter(Mandatory=$false, HelpMessage="The name of the environment variable.")]
+        [string]$Name
+    )
+
+    if ($Name) {
+        (Get-ChildItem env:$Name).Value
+        return
+    }
+
     Get-ChildItem env:* | Sort-Object name
 }
 
