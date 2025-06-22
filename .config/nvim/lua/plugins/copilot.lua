d7030834b4dcf6270caaf04dca2ce9a5b7f0b470
Shaun Chong <data-miner00@users.noreply.github.com>
2025-06-22 17:37:57 +0800

    style(nvim): Format copilot

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -1,11 +1,11 @@
 return {
-    'github/copilot.vim',
-    config = function()
-        -- For first time use, run `:Copilot setup` within neovim
-        -- This will generate a one-time password to authenticate with GitHub
-        -- Copy the password, hit 'Enter' as prompted and it will open GitHub authentication in browser
-        -- Paste the password and press 'Authenticate'
-        -- It should then show 'Congratulations, you're all set!' and it is done
-    end,
-    enabled = true,
+  "github/copilot.vim",
+  config = function()
+    -- For first time use, run `:Copilot setup` within neovim
+    -- This will generate a one-time password to authenticate with GitHub
+    -- Copy the password, hit 'Enter' as prompted and it will open GitHub authentication in browser
+    -- Paste the password and press 'Authenticate'
+    -- It should then show 'Congratulations, you're all set!' and it is done
+  end,
+  enabled = true,
 }
