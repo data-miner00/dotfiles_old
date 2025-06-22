5d6b6f4950af8dd1deb163d2f9f74f6201f6d0cd
Shaun Chong <data-miner00@users.noreply.github.com>
2025-06-22 23:20:25 +0800

    feat(nvim): Added dashboard

---
+++ .config/nvim/lua/plugins/dashboard.lua
@@ -0,0 +1,49 @@
+return {
+  "nvimdev/dashboard-nvim",
+  event = "VimEnter",
+  config = function()
+    require("dashboard").setup({
+      vim.api.nvim_set_hl(0, "DashboardHeader", { fg = "#5d5fd4" }),
+      theme = "doom",
+      config = {
+        center = {
+          {
+            action = "Lazy update",
+            desc = " Update Plugins",
+            icon = "󰚰 ",
+            key = "u",
+          },
+          {
+            action = "Telescope find_files",
+            desc = " Find file",
+            icon = " ",
+            key = "f",
+          },
+        },
+        header = {
+          "",
+          "",
+          "                                   ",
+          "                                   ",
+          "                                   ",
+          "   ⣴⣶⣤⡤⠦⣤⣀⣤⠆     ⣈⣭⣿⣶⣿⣦⣼⣆          ",
+          "    ⠉⠻⢿⣿⠿⣿⣿⣶⣦⠤⠄⡠⢾⣿⣿⡿⠋⠉⠉⠻⣿⣿⡛⣦       ",
+          "          ⠈⢿⣿⣟⠦ ⣾⣿⣿⣷    ⠻⠿⢿⣿⣧⣄     ",
+          "           ⣸⣿⣿⢧ ⢻⠻⣿⣿⣷⣄⣀⠄⠢⣀⡀⠈⠙⠿⠄    ",
+          "          ⢠⣿⣿⣿⠈    ⣻⣿⣿⣿⣿⣿⣿⣿⣛⣳⣤⣀⣀   ",
+          "   ⢠⣧⣶⣥⡤⢄ ⣸⣿⣿⠘  ⢀⣴⣿⣿⡿⠛⣿⣿⣧⠈⢿⠿⠟⠛⠻⠿⠄  ",
+          "  ⣰⣿⣿⠛⠻⣿⣿⡦⢹⣿⣷   ⢊⣿⣿⡏  ⢸⣿⣿⡇ ⢀⣠⣄⣾⠄   ",
+          " ⣠⣿⠿⠛ ⢀⣿⣿⣷⠘⢿⣿⣦⡀ ⢸⢿⣿⣿⣄ ⣸⣿⣿⡇⣪⣿⡿⠿⣿⣷⡄  ",
+          " ⠙⠃   ⣼⣿⡟  ⠈⠻⣿⣿⣦⣌⡇⠻⣿⣿⣷⣿⣿⣿ ⣿⣿⡇ ⠛⠻⢷⣄ ",
+          "      ⢻⣿⣿⣄   ⠈⠻⣿⣿⣿⣷⣿⣿⣿⣿⣿⡟ ⠫⢿⣿⡆     ",
+          "       ⠻⣿⣿⣿⣿⣶⣶⣾⣿⣿⣿⣿⣿⣿⣿⣿⡟⢀⣀⣤⣾⡿⠃     ",
+          "                                   ",
+          "",
+          "",
+          "",
+        },
+      },
+    })
+  end,
+  dependencies = { { "nvim-tree/nvim-web-devicons" } }, -- Optional, for icons
+}
